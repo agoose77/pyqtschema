f597373cf74b101fb244ce64ff36df96810cf539
Angus Hollands <goosey15@gmail.com>
2017-09-14 13:36:39 +0100

    Cleanups to main

--- qtjsonschema/__main__.py
+++ qtjsonschema/__main__.py
@@ -6,7 +6,10 @@ Generate a dynamic Qt form representing a JSON Schema.
 Filling the form will generate JSON.
 """
 
+import collections
+import json
 from json import dumps
+from pathlib import Path
 
 import click
 from PyQt5 import QtCore, QtWidgets
@@ -18,7 +21,7 @@ from .widgets import create_widget
 class MainWindow(QtWidgets.QWidget):
     schema = None
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, validation_interval=100):
         QtWidgets.QWidget.__init__(self, parent)
 
         self.setWindowTitle("PyQt JSON Schema Editor")
@@ -49,35 +52,16 @@ class MainWindow(QtWidgets.QWidget):
         self.schema_widget = None
         self.schema = None
 
-        label = QtWidgets.QLabel()
+        self._validation_label = QtWidgets.QLabel()
+        self._format_checker = FormatChecker()
 
-        def update():
-            if self.schema_widget is None:
-                return
-
-            format_checker = FormatChecker()
-            validator = Draft4Validator(self.schema, format_checker=format_checker)
-
-            errors = [err for err in validator.iter_errors(self.schema_widget.dump_json_object())]
-            if errors:
-                error = errors[0]
-                error_string = ("{} errors" if len(errors) > 1 else "{} error").format(len(errors))
-                label.setText("{}.\nFirst error in {}:\n{}".format(error_string, '#/' + '/'.join(error.schema_path),
-                                                                   error.message))
-                label.setStyleSheet("QLabel { color: red; }")
-
-            else:
-                label.setText("Object validates")
-                label.setStyleSheet("QLabel { color: green; }")
-
-        timer = QtCore.QTimer(self)
-        timer.setInterval(100)
-        timer.timeout.connect(update)
-        timer.start()
+        self._validation_timer = QtCore.QTimer(self)
+        self._validation_timer.setInterval(validation_interval)
+        self._validation_timer.timeout.connect(self._do_validation)
 
         vbox = QtWidgets.QVBoxLayout()
         vbox.addWidget(self.menu)
-        vbox.addWidget(label)
+        vbox.addWidget(self._validation_label)
         vbox.addWidget(self.content_region)
         vbox.setContentsMargins(0, 0, 0, 0)
 
@@ -87,39 +71,55 @@ class MainWindow(QtWidgets.QWidget):
 
         self.setLayout(hbox)
 
-    def process_schema(self, file_path):
+    @property
+    def format_checker(self) -> FormatChecker:
+        return self._format_checker
+
+    def load_schema(self, file_path):
         """
             Load a schema and create the root element.
         """
-        import json
-        import collections
-        from pathlib import Path
-
         schema_path = Path(file_path).absolute()
         with open(schema_path) as f:
-            _schema = json.loads(f.read(), object_pairs_hook=collections.OrderedDict)
+            schema = json.loads(f.read(), object_pairs_hook=collections.OrderedDict)
 
-        Draft4Validator.check_schema(_schema)
+        Draft4Validator.check_schema(schema)
 
-        if "title" in _schema:
-            self.setWindowTitle("%s - PyQtSchema" % _schema["title"])
-
-        self.schema_widget = create_widget(_schema.get("title", "(root)"), _schema, schema_path.as_uri())
+        schema_title = schema.get("title", "<root>")
+        self.setWindowTitle("{} - PyQt JSON Schema".format(schema_title))
+        self.schema_widget = create_widget(schema_title, schema, schema_path.as_uri())
         self.content_region.setWidget(self.schema_widget)
         self.content_region.setWidgetResizable(True)
-        self.schema = _schema
+        self.schema = schema
+
+        self._validation_timer.start()
 
     def load_json(self, json_file):
         """
             Load a schema and create the root element.
         """
-        import json
-        import collections
         with open(json_file) as f:
             data = json.loads(f.read(), object_pairs_hook=collections.OrderedDict)
             # from jsonschema import validate
             # validate(data, self.schema)
             self.schema_widget.load_json_object(data)
+
+    def _do_validation(self):
+        label = self._validation_label
+
+        validator = Draft4Validator(self.schema, format_checker=self._format_checker)
+
+        errors = [err for err in validator.iter_errors(self.schema_widget.dump_json_object())]
+        if errors:
+            error = errors[0]
+            error_string = ("{} errors" if len(errors) > 1 else "{} error").format(len(errors))
+            label.setText("{}.\nFirst error in {}:\n{}".format(error_string, '#/' + '/'.join(error.schema_path),
+                                                               error.message))
+            label.setStyleSheet("QLabel { color: red; }")
+
+        else:
+            label.setText("Object validates")
+            label.setStyleSheet("QLabel { color: green; }")
 
     def _handle_open_json(self):
         # Open JSON File
@@ -131,7 +131,7 @@ class MainWindow(QtWidgets.QWidget):
         # Open JSON Schema
         schema, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open Schema', filter="JSON Schema (*.schema *.json)")
         if schema:
-            self.process_schema(schema)
+            self.load_schema(schema)
 
     def _handle_save(self):
         # Save JSON output
@@ -158,7 +158,7 @@ def json_editor(schema, json):
     main_window.resize(1000, 800)
 
     if schema:
-        main_window.process_schema(schema)
+        main_window.load_schema(schema)
         if json:
             main_window.load_json(json)
 
